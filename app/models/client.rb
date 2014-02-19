4850c108a09cd506606bc0fff3d133fe15d1aad4
Warren Chaudhry <warrenchaudhry@gmail.com>
2014-02-19 21:09:08 +0800

    Fix client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,11 @@
 class Client
-  attr_accessible :client_id  
-  def initialize(connection)
-    @connection = connection
-  end
-  
-  def list
-    request_client = @connection.client.list
-    @clients => request_client['clients']['client'] 
-  end
+  # attr_accessible :client_id  
+  # def initialize(connection)
+    # @connection = connection
+  # end
+#   
+  # def list
+    # request_client = @connection.client.list
+    # @clients => request_client['clients']['client'] 
+  # end
 end
